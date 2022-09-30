215d617371c39ff588f10f9aeb72f8b55b40c472
Uran Hajrizaj <uranhajrizaj@outlook.com>
2022-09-30 10:17:09 +0200

    updated

--- src/components/Conversation.tsx
+++ src/components/Conversation.tsx
@@ -1,20 +1,59 @@
 import { useEffect, useState } from "react";
 import { BiSend } from "react-icons/bi"
 import io from "socket.io-client";
+// import Socket from "socket.io"
 
 export function Connversation({ selectedFriend, currentuser }: any) {
     const [messages, setMessages] = useState([])
     const [socket, setSocket] = useState<any>(null)
+    const [room, setRoom] = useState(null)
 
+    console.log("room:", room)
     useEffect(() => {
         const socket = io("ws://localhost:4555");
         setSocket(socket)
         console.log(socket)
-        socket.on("message",(messages)=>{
+        socket.on("message", (messages) => {
             setMessages(messages)
-        })     
+        })
     }, [])
-    
+
+    useEffect(() => {
+        fetch("http://localhost:4001/friendship", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                friend2Id: Number(selectedFriend.id),
+                friend1Id: Number(currentuser.id)
+            }),
+        })
+            .then((resp) => resp.json())
+            .then(data => {
+                if (data.room) setRoom(data.room)
+                else {
+                    fetch("http://localhost:4001/friendship", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+
+                            friend2Id: Number(currentuser.id),
+                            friend1Id: Number(selectedFriend.id)
+                        }),
+                    })
+                        .then(resp => resp.json())
+                        .then(data => setRoom(data.room))
+                }
+            })
+    }, [selectedFriend])
+    // useEffect(() => {
+    //     const socket = new Socket("ws://localhost:4555")
+    //     return () => socket.disconnect()
+    //   }, [])
+
     console.log(messages)
     return (
         <div className="conversation">
@@ -23,18 +62,23 @@ export function Connversation({ selectedFriend, currentuser }: any) {
                 <p>{selectedFriend.name}</p>
             </header>
             <div className='messsages'>
-                <div className='friend_messsage'>
-                    <ul className='messsages_list'>
-                    {messages.map((msg) => <li>{msg.user.name}: {msg.content}</li>
+                <ul className='messsages_list'>
+                    {messages.map((msg) => room === 1 ?
+                        <li className={msg.user.name === currentuser.name ? "my_message" : "friend_message"}>
+                            <div className={msg.user.name === currentuser.name ? "my_messagediv" : "friend_messagediv"}>
+                                <p className="message_sender">{msg.user.name}</p>
+                                <p className="message_content">{msg.content}</p>
+                            </div>
+                        </li>
+                        :
+                        null
                     )}
-                    </ul>
-                   
-                </div>
+                </ul>
             </div>
             <form className='send-message' onSubmit={(e) => {
                 e.preventDefault()
                 if (e.target.text.value) {
-                    socket.emit('message',{content:e.target.text.value, user: currentuser} );
+                    socket.emit('message', { content: e.target.text.value, user: currentuser });
                     e.target.text.value = '';
                 }
             }}>
